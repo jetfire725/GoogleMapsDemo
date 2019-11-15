699427b957a809c76bf013f5954e8357b70f0100
David Moore <dmoore42n81@gmail.com>
2019-11-15 14:40:34 -0500

    Geolocation

--- geolocation.js
+++ geolocation.js
@@ -1,12 +1,3 @@
-function initMap(){
-    let options = {
-        zoom: 6,
-        center: {lat: 35, lng: -79}
-    }
-
-    let map = new google.maps.Map(document.getElementById("map"), options)
-}
-
 
 let arrayOfGeofences = [];
 let arrayOfGeofenceArgumentClicks = [];
