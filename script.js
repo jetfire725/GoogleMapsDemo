7c0dc4eaab6e224032389792caebc782161a576f
tmam101 <thomaspgoss98@gmail.com>
2019-11-15 15:00:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,26 +11,10 @@ function initMap() {
       newLat = Math.floor(Math.random() * Math.floor(10))/10000
       newLong = Math.floor(Math.random() * Math.floor(10))/10000
       if (negative) {
-        newLat = 1 - newLat
-        newLong = 1 - newLong
+        newLat = 0 - newLat
+        newLong = 0 - newLong
       }
       otherLatLng.push({lat: myLatLng.lat+newLat, lng: myLatLng.lng+newLong})
     }
     console.log(otherLatLng)
-    for(var i = 0; i < 10; i++) {
-      var image =
-      {
-        url: 'https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c325.png',
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      }
-      var marker = new google.maps.Marker({
-        position: otherLatLng[i],
-        map: map,
-        title: 'Pikachu',
-        icon: image
-      });
-    }
 }
