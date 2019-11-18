7a2cdf3adf0e7fa2bc0b14136b465d5470b16987
tmam101 <thomaspgoss98@gmail.com>
2019-11-18 13:24:19 -0500

    Add comments, cleanup

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
+var myLatLng, options, map;
+
 function initMap() {
-    var myLatLng = {lat: 35.908161, lng: -79.051970};
-    let options = {
+    /// SETUP
+    myLatLng = {lat: 35.908161, lng: -79.051970};
+    options = {
         zoom: 17,
         center: myLatLng
     }
-    let map = new google.maps.Map(document.getElementById("map"), options)
-    otherLatLng = []
-    for(var i =0; i < 10; i++) {
-      negative = Math.floor(Math.random() * Math.floor(2))
-      newLat = Math.floor(Math.random() * Math.floor(10))/10000
-      newLong = Math.floor(Math.random() * Math.floor(10))/10000
-      if (negative) {
-        newLat = 0 - newLat
-        newLong = 0 - newLong
-      }
-      otherLatLng.push({lat: myLatLng.lat+newLat, lng: myLatLng.lng+newLong})
+    map = new google.maps.Map(document.getElementById("map"), options)
+    generateMarkers()
+}
+
+function generateMarkers() {
+  markers = []
+  // To create 10 markers nearby our original coordinates,
+  for(var i =0; i < 10; i++) {
+    // Create a latitude adjustment and longitude adjustment
+    // that we will apply to our original coordinates
+    latitudeAdjustment = randomNumber(10)/100000
+    longitudeAdjustment = randomNumber(10)/100000
+    // Create a 50% chance of making our latitude and longitude
+    // adjustments negative
+    if (randomNumber(2)) {
+      latitudeAdjustment = 0 - latitudeAdjustment
+      longitudeAdjustment = 0 - longitudeAdjustment
     }
-    console.log(otherLatLng)
+    // Use these values to create a new set of coordinates that is slightly offset from our original coordinates
+    let newLatLng = {lat: myLatLng.lat+latitudeAdjustment, lng: myLatLng.lng+longitudeAdjustment}
+    // Create a marker at these new coordinates
+    var marker = new google.maps.Marker({
+      position: newLatLng,
+      map: map,
+      title: 'Pikachu',
+    });
+    // Add it to our set of markers
+    markers.push(marker)
+  }
+}
+/// Returns a random number from 0 to max -1.
+function randomNumber(max) {
+  return Math.floor(Math.random() * Math.floor(max))
 }
