b74705b65adb0b52346cba5b2a9f5e479d69aeb5
Talitha van Lith <43021762+talithavanlith@users.noreply.github.com>
2019-11-18 15:09:57 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function initMap(){
-    let options = {
-        zoom: 6,
-        center: {lat: 35, lng: -79}
-    }
-
-    let map = new google.maps.Map(document.getElementById("map"), options)
-}
