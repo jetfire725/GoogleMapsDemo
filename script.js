45adcd65bb1b440ec32e528aaabe52684c1b1078
tmam101 <thomaspgoss98@gmail.com>
2019-11-18 15:03:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,19 +17,21 @@ function generateMarkers() {
   for(var i =0; i < 10; i++) {
     // TODO: Create a latitude adjustment and longitude adjustment
     // that we will apply to our original coordinates
-
+    latitudeAdjustment = undefined
+    longitudeAdjustment = undefined
     // TODO: Create a 50% chance of making our latitude and longitude
     // adjustments negative
-    if (randomNumber(2)) {
-
+    if (randomNumber(2) == 1) {
+      latitudeAdjustment = undefined
+      longitudeAdjustment = undefined
     }
     // TODO: Use these values to create a new set of coordinates that is slightly offset from our original coordinates
-    let newLatLng = 
+    let newLatLng = undefined
     // TODO: Create a marker at these new coordinates
     var marker = new google.maps.Marker({
-      position: ,
-      map: ,
-      title:
+      position: undefined,
+      map: undefined,
+      title: undefined
     });
     // TODO: Add it to our set of markers
   }
