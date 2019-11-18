2e89e4d322414ef9915804c1b5ea13d42e96df38
David Moore <dmoore42n81@gmail.com>
2019-11-18 15:16:25 -0500

    Added demo code

--- script.js
+++ script.js
@@ -1,73 +1,3 @@
-function initMap(){
-    let options = {
-        zoom: 6,
-        center: {lat: 35, lng: -79}
-    }
-
-    let map = new google.maps.Map(document.getElementById("map"), options)
-}
-
-
-let arrayOfGeofences = [];
-let arrayOfGeofenceArgumentClicks = [];
-class Geofence {
-    constructor() {
-
-        // Generalize constructor to also take in a single array? Useful for onclick?
-
-        if(arguments.length < 4) {
-            // error
-            // check if 1st arg is an array:
-            if(Array.isArray(arg[0])) {
-                // parse array. 
-                this.arguments = [...arguments[0]];
-            } else {
-                throw "ERROR. CANNOT CREATE GENFENCE WITHOUT AT LEAST 2 COORDINATES";
-            }
-            
-        }
-
-        this.coordinates = [...arguments]; // stores copy of passed in arguments. Important for referencing later.
-        
-
-        
-        
-        
-    }
-
-    get getCoordinates(){
-        return this.coordinates;
-    }
-
-    isUserInGeofence(givenLat, givenLon) {
-        // function to determine if a given coordinate is within the coordinates of the Geofence.
-        // check if range is undefined:
-        let latitudes = [];
-        let longitudes = [];
-        for(let i = 0; i < this.coordinates.length; i+=2) {
-            latitudes.push(this.coordinates[i]);
-            longitudes.push(this.coordinates[i+1]);
-        }
-
-        // Something is within the fence if the coordinates are between the min and max latitude and longitude.
-        
-        // find min lat and lon. Find max lat and lon. Check if given values are within those ranges.
-        let minLat = Math.min(latitudes);
-        let maxLat = Math.max(latitudes);
-        let minLon = Math.min(longitudes);
-        let maxLon = Math.max(longitudes);
-
-        if(givenLat >= minLat && givenLat <= maxLat && givenLon >= minLon && givenLon <= maxLon) {
-            return true;
-        } 
-        return false;
-
-
-        
-    }
-}
-
-
 async function initMap() {
     let wilsonHall = {lat: 35.9076, lng: -79.0518};
     
@@ -104,6 +34,60 @@ async function initMap() {
     
     console.log(g.coordinates); 
 }
+
+
+/* Given a set of coordinates, facilitates users to encapsulate a defined area as a Geofence. 
+Provides functionality to determine cross section of coordinates to establish if a given 
+coordinate falls within the Geofence. */
+
+class Geofence {
+    constructor() {
+
+        /* Write a constructor to encapsulate paired coordinates of (latitude,longitude) as 
+        the anchor points of the Geofence. Constructor should be able to take parameters of any 
+        length. All parameters should be numbers. */
+
+        // Encapsulate all coordinates where we could be passed any even number of coordinates
+        this.coordinates = [];
+
+        if(this.coordinates.length < 4) {
+            throw "EXCEPTION! CANNOT CREATE GENFENCE WITHOUT AT LEAST 2 COORDINATE PAIRS";
+        }
+        
+        
+    }
+
+    get getCoordinates(){
+        // Getter for coordinates. 
+        return this.coordinates;
+    }
+
+    isUserInGeofence(givenLat, givenLon) {
+        // function to determine if a given coordinate is within the coordinates of the Geofence.
+        let isWithin = false;
+
+        let latitudes = [];
+        let longitudes = [];
+        for(let i = 0; i < this.coordinates.length; i+=2) {
+            latitudes.push(this.coordinates[i]);
+            longitudes.push(this.coordinates[i+1]);
+        }
+
+        
+        // How can we determine if something is within the fence?
+
+        // Your Code Goes Here:
+        
+
+        
+
+
+        return isWithin;
+    }
+}
+
+
+
 
 
 
