fa97a45400520a130f3faeed1800549f4166ce79
talithavanlith <talithavanlith@outlook.com>
2019-11-19 17:00:59 -0500

    edited some errors and made the map bigger

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var map;
 function generateMarkers(myLatLng) {
     markers = []
         // To create 10 markers nearby our original coordinates,
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 20; i++) {
         // Create a latitude adjustment and longitude adjustment
         // that we will apply to our original coordinates
-        latitudeAdjustment = randomNumber(10) / 100000
-        longitudeAdjustment = randomNumber(10) / 100000
+        latitudeAdjustment = randomNumber(10) / 10000
+        longitudeAdjustment = randomNumber(10) / 10000
             // Create a 50% chance of making our latitude and longitude
             // adjustments negative
         if (randomNumber(2)) {
