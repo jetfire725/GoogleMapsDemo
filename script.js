347878caf2a9b40c90a9cf936417883f9688a099
tmam101 <thomaspgoss98@gmail.com>
2019-11-20 10:29:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,29 +22,27 @@ function generateMarkers(originalLatLng) {
   markers = []
   // To create 20 markers nearby our original coordinates,
   for(let i =0; i < 20; i++) {
-    // Create a latitude adjustment and longitude adjustment
+    // TODO: Create a latitude adjustment and longitude adjustment
     // that we will apply to our original coordinates
-    latitudeAdjustment = randomNumber(10)/10000
-    longitudeAdjustment = randomNumber(10)/10000
-    // Create a 50% chance of making our latitude and longitude
+    latitudeAdjustment = undefined
+    longitudeAdjustment = undefined
+    // TODO: Create a 50% chance of making our latitude and longitude
     // adjustments negative
-    if (randomNumber(2)) {
-      latitudeAdjustment = 0 - latitudeAdjustment
-      longitudeAdjustment = 0 - longitudeAdjustment
+    if (randomNumber(2) == 1) {
+      latitudeAdjustment = undefined
+      longitudeAdjustment = undefined
     }
-    // Use these values to create a new set of coordinates that is slightly offset from our original coordinates
-    let newLatLng = {lat: originalLatLng.lat+latitudeAdjustment, lng: originalLatLng.lng+longitudeAdjustment}
-    // Create a marker at these new coordinates
+    // TODO: Use these values to create a new set of coordinates that is slightly offset from our original coordinates
+    let newLatLng = undefined
+    // TODO: Create a marker at these new coordinates
     let marker = new google.maps.Marker({
-      position: newLatLng,
-      map: map,
-      title: 'Pokemon',
+      position: undefined,
+      map: undefined,
+      title: undefined
     });
-    // Add it to our set of markers
-    markers.push(marker)
+    // TODO: Add it to our set of markers
   }
 }
-
 /// Returns a random number from 0 to max-1.
 function randomNumber(max) {
   return Math.floor(Math.random() * Math.floor(max))
