2b5b083aa1c3b5a5bc8454ff268f2a4016a674c9
Ethan Bateman <jetfire725@hotmail.com>
2019-11-19 22:49:41 -0500

    gutted

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 function initMap(){
-    let options = {
-        zoom: 17,
-        center: {lat: 35.9076, lng: -79.0518}
-    }
 
-    let map = new google.maps.Map(document.getElementById("map"), options)
+    //TODO: DECLARE OPTIONS OBJECT, AND GOOGLE MAP OBJECT
+    
 }
